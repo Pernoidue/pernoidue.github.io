0bd7b8bf53ee4e7eb3d263ec1158813d37a640db
Pistorius-afk <marcelpotriveala@gmail.com>
2025-08-05 00:04:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var objPeople = [
+    {
+        username : "Mongola31",
+        password : "TerrazzaMichelangeloBarCentro6122023"
+    },
+] 
+var log=false;
+
+function getInfo() {
+    var username = document.getElementById('username').value
+    var password = document.getElementById('password').value
+
+    for(var i=0; i< objPeople.length;i++){
+        if(username== objPeople[i].username && password== objPeople[i].password){
+            window.location.href = 'http://127.0.0.1:5500/Home.html';
+            log=true;
+            return
+        }
+        
+    }
+    
+
+}   
+
+function ver(){
+    if(log){
+
+    }
+    else
+    {
+        window.location.href = 'http://127.0.0.1:5500/login.html';
+        return
+    }
+
+}
